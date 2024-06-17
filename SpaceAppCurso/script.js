ba38a29ec1b22a1dda93dba2bb0a546c6008ed90
AmyQuin <149535311+AmyQuin@users.noreply.github.com>
2024-06-17 14:34:18 -0600

    Tratando las promises
    Lo que aprendimos en esta aula:
    
    -Utilizar el método then para acceder al valor devuelto por la API fetch;
    -Manejar posibles errores con el método catch;
    -Construir funciones asíncronas con async;
    -Realizar solicitudes con await;
    -Manejar errores con try catch.

--- SpaceAppCurso/script.js
+++ SpaceAppCurso/script.js
@@ -1,13 +1,65 @@
 
-const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=5"
+const url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=15"
 
-fetch(url)
-.then()
-.catch()
+//ASYNC Y AWAIT
+async function listaImagenes() {
+    try{
+        let fetchImagen = await fetch(url)
+        let datosImagenes = await fetchImagen.json()
+
+        console.log(datosImagenes)
+        const card = document.querySelector("[data-ul]")
+
+        datosImagenes.forEach(elemento => {
+            const contenido = 
+            `<li class="card">
+            <img class="card__image" src="${elemento.url}" alt="imagen">
+            <h3 class="card__title">${elemento.title}</h3>
+            </li>
+            `
+
+            card.innerHTML = card.innerHTML + contenido
+        })
+        
+
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+listaImagenes()
+
+// THEN Y CATCH
+/*
+function listaImagenes(){
+    fetch(url)
+    .then(response => response.json())
+    .then(datosImagenes => {
+        console.log(datosImagenes)
+
+        const card = document.querySelector("[data-ul]")
+
+        datosImagenes.forEach(elemento => {
+            const contenido = `<li class="card">
+                    <img class="card__image" src="${elemento.url}" alt="imagen">
+                    <h3 class="card__title">${elemento.title}</h3>
+                </li>
+                `
+
+                card.innerHTML = card.innerHTML + contenido
+        })
+    })
+    .catch(error => console.log(error))
+}
+
+listaImagenes()
+*/
 
 
 
-//Ejemplo de como funciona una promesa
+
+/*Ejemplo de como funciona una promesa
 const solicitud = new Promise ((resolve, reject) => {
     const response = "resolve"
 
@@ -20,3 +72,4 @@ const solicitud = new Promise ((resolve, reject) => {
 })
 
 console.log(solicitud)
+*/
